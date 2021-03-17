e8bfb076f67c84d834278f86dbdbaf9962af7638
Melvin <31448155+melvio@users.noreply.github.com>
2021-03-17 18:34:31 +0100

    reset xhci controllers

--- reset-xhci-controllers.sh
+++ reset-xhci-controllers.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script to reset all local xHCI (USB) controllers
-# Based on: http://billauer.co.il/blog/2013/02/usb-reset-ehci-uhci-linux/
+# Based on: <http://billauer.co.il/blog/2013/02/usb-reset-ehci-uhci-linux/>
 
 if [[ ${EUID} != 0 ]]; then
-	echo This must be run as root!
-	exit 1
+  echo "This script must be run as root"
+  exit 1
 fi
 
 for xhci in /sys/bus/pci/drivers/?hci_hcd; do
-	if ! cd ${xhci}; then
-		echo "Weird error. Failed to change directory to ${xhci}."
-	exit 1
-	fi
-	echo "Resetting devices from ${xhci}..."
-	for i in ????:??:??.?; do
-		echo -n "${i}" > unbind
-		echo -n "${i}" > bind
-	done
+  if ! cd "${xhci}"; then
+    echo "Failed to change directory to ${xhci}."
+    exit 1
+  fi
+
+  echo "Resetting devices from ${xhci}..."
+  for i in ????:??:??.?; do
+    echo -n "${i}" > unbind
+    echo -n "${i}" > bind
+  done
+
 done
